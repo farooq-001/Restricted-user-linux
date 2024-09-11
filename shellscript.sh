49ccec68b91cb43d2f25d70ce32a0a5752c910c2
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-09-11 10:02:30 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-# Create the user with a home directory
-sudo useradd -m ch_uk
+# Prompt for the username
+read -p "Enter the username: " username
 
-# Set a password for the user (you can modify the echo command or remove it if you prefer to set the password interactively)
-echo "ch_uk:password" | sudo chpasswd
+# Create the user with a home directory
+sudo useradd -m $username
+
+# Prompt for the password (password input will be hidden)
+read -sp "Enter the password for $username: " password
+echo
+
+# Set the password for the user
+echo "$username:$password" | sudo chpasswd
 
 # Change the user's shell to the restricted shell
-sudo chsh -s /bin/rbash ch_uk
+sudo chsh -s /bin/rbash $username
 
 # Create a bin directory in the user's home directory for allowed commands
-sudo mkdir /home/ch_uk/bin
+sudo mkdir /home/$username/bin
 
 # Create symbolic links for allowed commands
-sudo ln -s /bin/df /home/ch_uk/bin/df
-sudo ln -s /usr/bin/htop /home/ch_uk/bin/htop
-sudo ln -s /bin/ls /home/ch_uk/bin/ls
+sudo ln -s /bin/df /home/$username/bin/df
+sudo ln -s /usr/bin/htop /home/$username/bin/htop
+sudo ln -s /bin/ls /home/$username/bin/ls
 
 # Set the PATH to only include the restricted bin directory
-echo 'export PATH=/home/ch_uk/bin' | sudo tee -a /home/ch_uk/.bashrc
+echo "export PATH=/home/$username/bin" | sudo tee -a /home/$username/.bashrc
 
 # Set ownership and permissions for the bin directory
-sudo chown -R ch_uk:ch_uk /home/ch_uk/bin
-sudo chmod 750 /home/ch_uk/bin
+sudo chown -R $username:$username /home/$username/bin
+sudo chmod 750 /home/$username/bin
 
-echo "User 'ch_uk' has been created with restricted access to df -h, htop, and ls -lrth commands."
+echo "User '$username' has been created with restricted access to df -h, htop, and ls -lrth commands."
